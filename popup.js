30cb81a647c482f1f1b6bb659a7b2b926c7e0b54
Anthony Asp <46567012+AAspCodes@users.noreply.github.com>
2020-02-16 10:42:09 -0800

    jsq result handeling

--- popup.js
+++ popup.js
@@ -31,44 +31,39 @@ $(document).ready(function() {
         if (matches && matches.length) {
             $("#noanswers").hide();
 
-            var answers = document.getElementById('answers');
             for ( var i = 0; i<matches.length; i++) {
                 // get one entry at a time
                 var entry = matches[i];
                 // break up the names and capitalize them
                 var firstname = entry[0];
                 var lastname = entry[1];
+
                 firstname = firstname.charAt(0).toUpperCase() + firstname.substring(1);
                 lastname = lastname.charAt(0).toUpperCase() + lastname.substring(1);
                 var name = firstname + " " + lastname;
 
+                $("#answers").append(`<p id='name'> ${name} <p>`);
 
                 // convert rating string to a number for comparison
                 var rating = parseFloat(entry[2]);
-
-                // create html tag for the name
-                var namediv = document.createElement("p");
-                var node = document.createTextNode(name)
-                namediv.appendChild(node)
-                namediv.id = 'name';
-                answers.appendChild(namediv);
+                var color = "blue";
+                if (3.0 >= rating) {
+                    color = "red";
+                } else if (4.0 >= rating) {
+                    color = "#FFC840";
+                } else {
+                    color = "green";
+                }
 
                 // create html tag for the rating
-                var ratingresult = document.createElement("p");
-                ratingresult.id = 'rating';
-                if (3.0 >= rating) {
-                    ratingresult.style.color = "red";
-                } else if (4.0 >= rating) {
-                    ratingresult.style.color = "#FFC840";
-                } else {
-                    ratingresult.style.color = "green";
-                }
-                var node = document.createTextNode(entry[2])
-                ratingresult.appendChild(node);
-                answers.appendChild(ratingresult);
+                $("#answers").append(`<p id="rating" style="color: ${color}">${entry[2]}</p>`);
             }
         } else {
-            $("#noanswers").show()
+            $("#answers").empty();
+            $("#noanswers").show();
+
+        };
+    };
 
         }
     }
